fe075b1747fd7fcba6a68425403efd1f1f7e11e6
위정우 <80455444+yellow0104@users.noreply.github.com>
2021-10-04 16:09:30 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# 정우제작
+import random
+import time
+print("구구단을 외자~")
+time.sleep(1.2)
+print("구구단을 외자~")
+one_number = random.randint(12345, 99999999999)
+two_number = random.randint(12345, 99999999999)
+print(f"{one_number} x {two_number} 는?")
+answer = one_number * two_number
+answer = str(answer)
+print(answer)
+inp = input("정답은>")
+if inp == answer:
+    print("정답입니다")
+else:
+    print("오답입니다")
